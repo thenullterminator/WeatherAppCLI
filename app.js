fed2c05b266c594324323e86b99f6573a4959f2d
thenullterminator <dazz2803@gmail.com>
2019-05-19 19:05:53 +0530

    CALL BACK ERRORS

--- app.js
+++ app.js
@@ -21,9 +21,20 @@ request({
     json:true
 },(error,response,body)=>
 {
+    if(error)
+    {
+        console.log("Unable to connect to google servers.");
+    }
+    else if(body.status==="ZERO_RESULTS")
+    {
+        console.log("Unable to find that address");
+    }
+    else if(body.status==="OK")
+    {
     console.log("Address: "+body.results[0].formatted_address);
     console.log("Latitude: "+body.results[0].geometry.location.lat);
     console.log("Longitude: "+body.results[0].geometry.location.lng);
+    }
 });
 
 console.log(arg.address);
